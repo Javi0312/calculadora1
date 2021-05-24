97e92065e4fa370f0b3263d0b36b23063bf02ec6
Javi <Javi@Smoky>
2021-05-24 15:45:51 -0500

    app

--- src/main/java/com/javi/Calculadora.java
+++ src/main/java/com/javi/Calculadora.java
@@ -2,20 +2,21 @@ package com.javi;
 
 public class Calculadora {
 	
-	private int num1;
-	private int num2;
+	private double num1;
+	private double num2;
 	private char operador;
-	private float resultado;
-	public int getNum1() {
+	private double resultado;
+	
+	public double getNum1() {
 		return num1;
 	}
-	public void setNum1(int num1) {
+	public void setNum1(double num1) {
 		this.num1 = num1;
 	}
-	public int getNum2() {
+	public double getNum2() {
 		return num2;
 	}
-	public void setNum2(int num2) {
+	public void setNum2(double num2) {
 		this.num2 = num2;
 	}
 	public char getOperador() {
@@ -24,10 +25,10 @@ public class Calculadora {
 	public void setOperador(char operador) {
 		this.operador = operador;
 	}
-	public float getResultado() {
+	public double getResultado() {
 		return resultado;
 	}
-	public void setResultado(float resultado) {
+	public void setResultado(double resultado) {
 		this.resultado = resultado;
 	}
 	
